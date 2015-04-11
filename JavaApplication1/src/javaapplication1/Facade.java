8cb6cc2a4c1af689629a5c4f2d869497c7c7199b
Rafael Chacón Fallas <rafangel1228@gmail.com>
2015-04-11 09:56:01 -0600

    metodos para realizar operaciones

--- JavaApplication1/src/javaapplication1/Facade.java
+++ JavaApplication1/src/javaapplication1/Facade.java
@@ -8,15 +8,44 @@ package javaapplication1;
 public class Facade {
     private Poli _poli1;
     private Poli _poli2;
+    private String _operador;
     
     public Facade(){
         _poli1 = new Poli();
         _poli2 = new Poli();
+        _operador = "";
     }
    
     public void borrarOperandos(){
         _poli1 = new Poli();
         _poli2 = new Poli();
+    }
+    
+    public void setOperador(String poperador){
+        _operador = poperador;
+    }
+    
+    public String resolver(){
+        if(_poli1.getPoli()!=null && _poli2.getPoli()!=null && !_operador.equals("")){
+            //resolver la operacion guardando el resultado en operando1 y borrando el 2do
+        }
+        else{
+            //error
+        }
+        return "";
+    }
+    
+    public void eval(String pvalores){
+        if(_poli1.getPoli()!=null && _poli2.getPoli()==null){
+            String[] vals = pvalores.split(" ");
+            if(vals.length == 2){
+                //evaluo
+            }
+            else{
+                //error
+            }
+        }
+        
     }
     
     public void nuevoOperando(String poperando){
